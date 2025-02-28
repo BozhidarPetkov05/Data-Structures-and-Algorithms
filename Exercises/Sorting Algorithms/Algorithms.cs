a80da57f595234f00bdd54278be3ad52211369b0
Bozhidar Petkov <xidz01bog@gmail.com>
2025-02-28 20:34:11 +0200

    Added sorting algorithms

---
+++ Exercises/Sorting Algorithms/Algorithms.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Algorithms
+{
+    public static class Algorithms
+    {
+        public static int[] BubbleSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                bool swapped = false;
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                if (swapped == false)
+                {
+                    break;
+                }
+            }
+            return arr;
+        }
+
+        public static int[] SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                } 
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ Exercises/Sorting Algorithms/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Sorting_Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortingChecker("asd");
+            SortingChecker("Bubble Sort");
+            SortingChecker("Selection Sort");   
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static int[] ArrayFiller()
+        {
+            int Min = 0;
+            int Max = 10000;
+
+            int[] arr = new int[40000];
+            Random randNum = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = randNum.Next(Min, Max);
+            }
+
+            return arr;
+        }
+
+        public static void TimeCheck(long time)
+        {
+            Console.WriteLine("Runtime: " + time + "ms");
+        }
+
+        public static void SortingChecker(string algorithm)
+        {
+            switch (algorithm)
+            {
+                case "Bubble Sort":
+                    int[] bubbleSorted = ArrayFiller();
+                    Console.WriteLine("Generated Bubble Sort...");
+
+                    var watch = System.Diagnostics.Stopwatch.StartNew();
+                    Algorithms.BubbleSort(bubbleSorted);
+                    watch.Stop();
+                    TimeCheck(watch.ElapsedMilliseconds);
+                    watch.Reset();
+                    break;
+
+                case "Selection Sort":
+                    int[] selectionSorted = ArrayFiller();
+                    Console.WriteLine("Generated Selection Sort...");
+
+                    watch = System.Diagnostics.Stopwatch.StartNew();
+                    Algorithms.SelectionSort(selectionSorted);
+                    watch.Stop();
+                    TimeCheck(watch.ElapsedMilliseconds);
+                    watch.Reset();
+                    break;
+
+                default:
+                    Console.WriteLine("There is no such algorithm!");
+                    break;
+            }
+        }
+    }
+}
