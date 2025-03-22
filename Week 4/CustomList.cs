5a5c084a9084a69ac2df841e95169d18cbb1b256
Bozhidar Petkov <xidz01bog@gmail.com>
2025-03-22 15:44:27 +0200

    Linked list added

---
+++ Week 4/CustomList.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_4
+{
+    public class CustomList<T>
+    {
+        private T[] arr { get; set; }
+        private int count;
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public CustomList()
+        {
+            arr = new T[0];
+            count = 0;
+        }
+
+        public void Insert(T elm, int index)
+        {
+            if (index < 0 || index > count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            int length = arr.Length + 1;
+            T[] newArr = new T[length];
+            int tracker = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (i == index)
+                {
+                    newArr[i] = elm;
+                }
+                else
+                {
+                    newArr[i] = arr[tracker];
+                    tracker++;
+                }
+            }
+            count++;
+            arr = newArr;
+        }
+
+        public void Add(T elm)
+        {
+            Insert(elm, arr.Length);
+        }
+
+        public int FirstIndexOf(T elm)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Equals(elm))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index > count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            int length = arr.Length - 1;
+            T[] newArr = new T[length];
+            bool isSkipped = false;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (i == index)
+                {
+                    isSkipped = true;
+                }
+                if (isSkipped)
+                {
+                    newArr[i] = arr[i + 1];
+                }
+                else
+                {
+                    newArr[i] = arr[i];
+                }
+            }
+
+            count--;
+            arr = newArr;
+        }
+
+        public void Remove(T elm)
+        {
+            RemoveAt(FirstIndexOf(elm));
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return arr[index];
+            }
+            set
+            {
+                arr[index] = value;
+            }
+        }
+    }
+}
--- Week 4/Program.cs
+++ Week 4/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,98 @@ namespace Week_4
     {
         static void Main(string[] args)
         {
+            /*
+            int num = 13;
+            ToBinary(num);
+            ToNumberSystem(28, 16);
+
+            CustomList<int> customList = new CustomList<int>();
+
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+            customList.Add(4);
+            customList.Add(5);
+            customList.Add(6);
+
+            customList.Insert(40, 2);
+            customList.RemoveAt(0);
+            customList.Remove(5);
+
+            Console.WriteLine(customList.Count);
+            Console.WriteLine(customList[0]);
+
+            for (int i = 0; i < customList.Count; i++)
+            {
+                Console.Write(customList[i] + " ");
+            }
+            Console.WriteLine();
+            */
+
+            LinkedList<int> linkedList = new LinkedList<int>();
+            
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            linkedList.Add(4);
+            linkedList.Add(5);
+            linkedList.Add(6);
+
+            //linkedList.Remove(3);
+            //linkedList.RemoveAt(1);
+            linkedList.PrintList();
+        }
+
+        public static void ToBinary(int num)
+        {
+            int startNum = num;
+            string binary = "";
+
+            while (num != 0)
+            {
+                int remainder = num % 2;
+                num /= 2;
+                binary += remainder;
+            }
+
+            string finalBinary = "";
+            for (int i = binary.Length - 1; i >= 0; i--)
+            {
+                finalBinary += binary[i];
+            }
+
+            Console.WriteLine($"{startNum} -> {finalBinary}");
+        }
+
+        public static void ToNumberSystem(int num, int divisor)
+        {
+            int startNum = num;
+            string code = "";
+
+            while (num != 0)
+            {
+                int remainder = num % divisor;
+
+                if (remainder > 9)
+                {
+                    char letter = Convert.ToChar(remainder + 55);
+                    code += letter;
+                }
+                else
+                {
+                    code += remainder;
+                }
+
+                num /= divisor;
+            }
+
+            string finalCode = "";
+            for (int i = code.Length - 1; i >= 0; i--)
+            {
+                finalCode += code[i];
+            }
+
+            Console.WriteLine($"{startNum} -> {finalCode}");
         }
     }
 }
